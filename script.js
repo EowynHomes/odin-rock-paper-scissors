dbbad695e3afbe451e2111ad94a5cd45c3861187
EowynHomes <248869830+EowynHomes@users.noreply.github.com>
2025-12-25 04:40:16 -0500

    add basic scaffolding pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Hello World!");
+//GET the computer's choice (Rock, paper, or scissors) 
+function getComputerChoice(){
+    //randomly select and return "rock", "paper", or "scissors".
+    //test what the function returns using console.log()
+}
+    
+
+//GET the user's choice (rock, paper, or scissors)
+    function getHumanChoice(){
+        //PROMPT the user
+        //(just assume the user *will* enter a valid choice)
+        //test what the function returns using console.log()
+    }
+    
